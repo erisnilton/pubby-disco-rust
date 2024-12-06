3ad259cf4d9aacc19124ac094c4e2e6cc6bdfed2
Salomão Neto <contato@sallon.dev>
2024-12-06 15:14:38 -0300

    refactor: update filter and search macros to use crate scope for count and filter_params

--- packages/api/src/infra/sqlx/filter_macro.rs
+++ packages/api/src/infra/sqlx/filter_macro.rs
@@ -1,28 +1,28 @@
 #[macro_export]
 macro_rules! filter_params {
-    ($qb: expr, $query:expr, {
-      $($name:ident($value:ident) => $filter:expr,)*
-    }) => {
-      let mut has_filters = false;
+  ($qb: expr, $query:expr, {
+    $($name:ident($value:ident) => $filter:expr,)*
+  }) => {
+    let mut has_filters = false;
 
-      $(
-        if let Some(value) = &$query.$name {
-          if has_filters {
-            $qb.push(" AND ");
-          }
-
-          $qb.push("(");
-          {
-            let $value = value;
-            $filter;
-          }
-          $qb.push(")");
-
-          #[allow(unused_assignments)]
-          {has_filters = true}
+    $(
+      if let Some(value) = &$query.$name {
+        if has_filters {
+          $qb.push(" AND ");
         }
-      )*
-    };
+
+        $qb.push("(");
+        {
+          let $value = value;
+          $filter;
+        }
+        $qb.push(")");
+
+        #[allow(unused_assignments)]
+        {has_filters = true}
+      }
+    )*
+  };
 }
 
 #[macro_export]
@@ -30,30 +30,30 @@ macro_rules! where_filter {
   ($qb: expr, $query:expr, {
     $($name:ident($value:ident) => $filter:expr,)*
   }) => {
-      if $($query.$name.is_some() || )* false {
-        $qb.push(" WHERE ");
-      }
+    if $($query.$name.is_some() || )* false {
+      $qb.push(" WHERE ");
+    }
 
-      filter_params! {
-        $qb,
-        $query,
-        {
-          $($name($value) => $filter,)*
-        }
+    $crate::filter_params! {
+      $qb,
+      $query,
+      {
+        $($name($value) => $filter,)*
       }
+    }
   };
 }
 
 #[macro_export]
 macro_rules! count {
     () => { 0 };
-    ($head:tt $($tail:tt)*) => { 1 + count!($($tail)*) };
+    ($head:tt $($tail:tt)*) => { 1 + $crate::count!($($tail)*) };
 }
 
 #[macro_export]
 macro_rules! search_by {
   ($qb:expr, $value:expr, $($field:literal$(,)?)*$(,)?) => {
-    let mut count = count!($($field)*);
+    let mut count = $crate::count!($($field)*);
 
     $(
       $qb.push($field);
@@ -73,17 +73,17 @@ macro_rules! search_by {
 
 #[macro_export]
 macro_rules! create_filter {
-    ($query:ty, $qb: ident => {
-      $($name:ident($value:ident) => $filter:expr,)*
-    }) => {
-        |$qb: &mut sqlx::QueryBuilder<'_, sqlx::Postgres>, query: &$query| {
-            where_filter! {
-              $qb,
-              query,
-              {
-                $($name($value) => $filter,)*
-              }
-            }
+  ($query:ty, $qb: ident => {
+    $($name:ident($value:ident) => $filter:expr,)*
+  }) => {
+    |$qb: &mut sqlx::QueryBuilder<'_, sqlx::Postgres>, query: &$query| {
+      $crate::where_filter! {
+        $qb,
+        query,
+        {
+          $($name($value) => $filter,)*
         }
-    };
+      }
+    }
+  };
 }
